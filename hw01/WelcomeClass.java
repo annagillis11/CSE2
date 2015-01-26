3940a8efe4642067fb3dd38f73a450c3661c6c9b
annagillis11 <annagillis11@gmail.com>
2015-01-26 21:02:39 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Anna Gillis
+//hw01
+//WelcomeClass Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+  public static void main(String[] args) {
+      
+// print the statement with box around it
+    System.out.println("    -----------");
+    System.out.println("    | WELCOME |");
+    System.out.println("    -----------");
+    System.out.println("   ^  ^  ^  ^  ^  ^");
+    System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--H--G--2--1--7->");
+    System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("   v  v  v  v  v  v");
+    System.out.println("My name is Anna Gillis and I'm 20 years old. I'm an art and engineering major.");
+    
+    //test/testtest
+  }
+}
